601fdc13c156b837f19eaf74e13529742f39ff65
Taijul Islam <taijulislam.st9@gmail.com>
2023-11-02 15:36:52 +0600

    fix(checkbox): selecting

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Image = ({ product }) => {
   const { id, name, img } = product;
+  useEffect(() => {
+    const handleCheckboxMouseDown = (event) => {
+      // Prevent drag behavior when interacting with the checkbox
+      event.stopPropagation();
+    };
+
+    // Attach the event listener when the component mounts
+    const checkbox = document.getElementById(`checkbox-${id}`);
+    checkbox.addEventListener("mousedown", handleCheckboxMouseDown);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      checkbox.removeEventListener("mousedown", handleCheckboxMouseDown);
+    };
+  }, [id]);
   return (
     <div
-      className=" border rounded-lg overflow-hidden shadow relative"
+      className=" border rounded-lg overflow-hidden shadow relative group"
       draggable
       key={id}
     >
+      <div className="overlay"></div>
       <label htmlFor={`product_${id}`}>
         <div>
           <img src={img} alt={name} className="w-full" />
@@ -16,8 +32,8 @@ const Image = ({ product }) => {
       <input
         type="checkbox"
         name="image"
-        id={`product_${id}`}
-        className="absolute top-2 left-2"
+        className="absolute top-2 left-2 cursor-pointer z-50"
+        id={`checkbox-${id}`}
       />
     </div>
   );
